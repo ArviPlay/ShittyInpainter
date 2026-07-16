b89f2ccfe172a073a888d9f5bd1110f238cfd368
Arvi <156504600+ArviPlay@users.noreply.github.com>
2026-07-16 15:48:02 +0300

    add ImageHelper with BitmapToArray method

---
+++ ImageHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace ShittyInpainter
+{
+    public static class ImageHelper
+    {
+        public static Color[,] BitmapToArray(Bitmap bmp)
+        {
+            Color[,] arr = new Color[bmp.Width, bmp.Height];
+
+            for(int x = 0; x < bmp.Width; x++)
+            {
+                for(int y = 0; y < bmp.Height; y++)
+                {
+                    arr[x, y] = bmp.GetPixel(x, y);
+                }
+            }
+
+            return arr;
+        }
+    }
+}
