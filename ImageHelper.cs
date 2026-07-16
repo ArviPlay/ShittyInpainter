4d6d1c18c74c0149e20cb09870111d8654138011
Arvi <156504600+ArviPlay@users.noreply.github.com>
2026-07-16 15:57:10 +0300

    add ArrayToBitmap method

--- ImageHelper.cs
+++ ImageHelper.cs
@@ -16,5 +16,18 @@
 
             return arr;
         }
+
+        public static Bitmap ArrayToBitmap(Color[,] arr)
+        {
+            Bitmap bmp = new Bitmap(arr.GetLength(0), arr.GetLength(1));
+
+            for (int x = 0; x < arr.GetLength(0); x++)
+            {
+                for (int y = 0; y < arr.GetLength(1); y++)
+                {
+                    bmp.SetPixel(x, y, arr[x, y]);
+                }
+            }
+        }
     }
 }
